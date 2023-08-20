f19afdee2057131a3aeb6580a68ccf2eb6add974
Ranyrety <mihau.ruminski@gmail.com>
2023-08-20 23:30:16 +0200

    simplest layout to see how it looks and compare with docking

--- gui/main.cpp
+++ gui/main.cpp
@@ -8,42 +8,50 @@ int main(int , char *[])
     runnerParams.appWindowParams.windowTitle = "M6800 - emulator gui demo";
     runnerParams.appWindowParams.windowGeometry.size = { 800,600 };
 
-    // ImGui window params
-    runnerParams.imGuiWindowParams.defaultImGuiWindowType =
-        HelloImGui::DefaultImGuiWindowType::ProvideFullScreenDockSpace;
-    runnerParams.imGuiWindowParams.showMenuBar = true;
-    runnerParams.imGuiWindowParams.showStatusBar = true;
+    runnerParams.callbacks.ShowGui =  [] {
+        // Left
+            static int selected = 0;
+        {
+            ImGui::BeginChild("left pane", ImVec2(ImGui::GetWindowWidth() * 0.40, -ImGui::GetFrameHeightWithSpacing()), true);
+            for (int i = 0; i < 100; i++)
+            {
+                // FIXME: Good candidate to use ImGuiSelectableFlags_SelectOnNav
+                char label[128];
+                sprintf(label, "MyObject %d", i);
+                if (ImGui::Selectable(label, selected == i))
+                    selected = i;
+            }
+            ImGui::EndChild();
+        }
+        ImGui::SameLine();
 
-    runnerParams.dockingParams.dockingSplits = {
-        {"Code", "Status", ImGuiDir_Left, 0.60f},
-    };
-    HelloImGui::DockableWindow dock_imguiDemoWindow;
-    {
-        dock_imguiDemoWindow.label = "Dear ImGui Demo";
-        dock_imguiDemoWindow.dockSpaceName = "Code";// This window goes into "MainDockSpace"
-        dock_imguiDemoWindow.GuiFunction = [&dock_imguiDemoWindow] {
-            if (dock_imguiDemoWindow.isVisible)
-                ImGui::ShowDemoWindow(nullptr);
+        // Right
+        {
+            ImGui::BeginGroup();
+            ImGui::BeginChild("item view", ImVec2(0, -ImGui::GetFrameHeightWithSpacing())); // Leave room for 1 line below us
+            ImGui::Text("MyObject: %d", selected);
+            ImGui::Separator();
+            if (ImGui::BeginTabBar("##Tabs", ImGuiTabBarFlags_None))
+            {
+                if (ImGui::BeginTabItem("Description"))
+                {
+                    ImGui::TextWrapped("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
+                    ImGui::EndTabItem();
+                }
+                if (ImGui::BeginTabItem("Details"))
+                {
+                    ImGui::Text("ID: 0123456789");
+                    ImGui::EndTabItem();
+                }
+                ImGui::EndTabBar();
+            }
+            ImGui::EndChild();
+            if (ImGui::Button("Revert")) {}
+            ImGui::SameLine();
+            if (ImGui::Button("Save")) {}
+            ImGui::EndGroup();
+        }
         };
-        dock_imguiDemoWindow.callBeginEnd = false;
-        dock_imguiDemoWindow.isVisible = true;
-    };
-
-    HelloImGui::DockableWindow dock_imguiDemoCode;
-    {
-        dock_imguiDemoCode.label = "CPU status";
-        dock_imguiDemoCode.dockSpaceName = "Status";// This window goes into "CodeSpace"
-        dock_imguiDemoCode.isVisible = true;
-        dock_imguiDemoCode.GuiFunction = [] { ImGui::Text("Here will be all status registers and memory"); };
-        dock_imguiDemoCode.imGuiWindowFlags = ImGuiWindowFlags_HorizontalScrollbar;
-        dock_imguiDemoCode.callBeginEnd = true;
-        dock_imguiDemoCode.windowSize = ImVec2(400, 600);
-    };
-
-    runnerParams.dockingParams.dockableWindows = {
-        dock_imguiDemoWindow,
-        dock_imguiDemoCode
-    };
     HelloImGui::Run(runnerParams);
     return 0;
 }
